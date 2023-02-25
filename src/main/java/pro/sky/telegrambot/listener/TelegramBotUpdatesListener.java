36116eafd1ebd70332c6969663fe688d066f2062
xandrosanchez <din.myrk@gmail.com>
2023-02-25 23:03:52 +0300

    Возвращена аннотация Autowired Удалены приватные данные из application.properties

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -21,6 +21,7 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
 
     private final Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
 
+    @Autowired
     private TelegramBot telegramBot;
 
     @PostConstruct
