185cca32f1f8ad369745f1583c1925b884c49330
IgorBerger03 <igor.berger98@yandex.ru>
2023-02-27 20:04:46 +0500

    добавил контроллеры + комментарии к ним

---
+++ src/main/java/pro/sky/telegrambot/controller/ClientController.java
@@ -0,0 +1,12 @@
+package pro.sky.telegrambot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// Controller для пользователя.
+// Предназначен для добавления, редактирования и удаления пользователя (усыновителя).
+
+@RestController
+@RequestMapping("/client")
+public class ClientController {
+}
